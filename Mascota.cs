12378216d9260feb700c0ea211e4c9d309d0f669
FedericoJSosa <federico13javiersosa@gmail.com>
2024-09-04 15:51:33 -0300

    Agregar archivos de proyecto.

---
+++ Mascota.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepasoObjetos
+{
+    public class Mascota
+    {
+        public Mascota(string nombre, string dueño)
+        {
+            Nombre = nombre;
+            Dueño = dueño;
+        }
+        public string Nombre { get; set; }
+        public string Dueño { get; set; }
+
+        public override string ToString()
+        {
+            return Nombre + "[" + Dueño + "]";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using RepasoObjetos;
+
+object m = new object();
+object m2 = new Mascota("nombre1","dueño1");
+object V = new Veterinario(2, "nombreVeterinario");
+
+object[] array = { m, m2, V };
+
+foreach (object objeto in array)
+{
+    Console.WriteLine(objeto.ToString());
+}
